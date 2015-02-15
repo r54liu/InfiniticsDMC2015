51a26c83b58b192799d3ccd80de1b2e6175571a9
sleepychina <debaiabed@gmail.com>
2015-02-14 19:21:40 -0500

    language

---
+++ language.r
@@ -0,0 +1,18 @@
+#function takes a column filled with E's and F's and gives a list of 1's and 0's
+
+language<-function(column){
+  
+  masterList<-c()
+  i=1
+  for(i in 1:length(column)){
+    if(column[i]=='E'){
+      masterList<-c(masterList, 1)
+    } else {
+      masterList<-c(masterList, 0)
+    }
+  }
+  masterList
+}
+
+  
+
