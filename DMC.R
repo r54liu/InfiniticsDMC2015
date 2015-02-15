9cc5d70664f8305bac3f01880ccf694e972050e7
liangliangni <llni@uwaterloo.ca>
2015-02-15 15:52:38 -0500

    Update DMC.R

--- DMC.R
+++ DMC.R
@@ -11,3 +11,34 @@ AD_ID.levels <- AD_ID.levels[AD_ID.levels != ""]                    # 13341 fact
 
 HEADLINE.levels <- levels(HEADLINE)
 HEADLINE.levels <- HEADLINE.levels[HEADLINE.levels != ""]           # 26 factors
+
+
+# Creating indicator variables for each keywords
+
+KEYWD_TXT <- train.click$KEYWD_TXT
+KEYWD_TXT <- levels(KEYWD_TXT)
+keyword <- unlist(strsplit(KEYWD_TXT, "[+]"))
+keyword <- keyword[keyword !=""]
+keyword <- unique(keyword)
+
+KEYWD_TXT <- paste(KEYWD_TXT, "+", sep= "")
+keyword <-paste(keyword, "+", sep="")
+
+keyword.indicator <- matrix (keyword, ncol=length(keyword), nrow = 1)
+
+j = 1
+while (j <= length(KEYWD_TXT)) {
+count = c()
+i = 1
+while (i <= length(keyword)) {
+  if (grepl(keyword[i], KEYWD_TXT[j])) {
+    count <- append (count, 1)
+  } else {
+    count <- append (count, 0)}
+i <- i+1
+}
+keyword.indicator <- rbind(keyword.indicator, count)
+j = j+1
+}
+
+# end of creating keyword indicators
