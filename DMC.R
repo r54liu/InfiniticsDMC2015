599ac7b4ed9097ba93b79a8f060d60b0d1417732
r54liu <r54liu@uwaterloo.ca>
2015-02-14 19:12:31 -0500

    Update DMC.R

--- DMC.R
+++ DMC.R
@@ -6,4 +6,8 @@ AD_ID <- as.factor(build$AD_ID)
 HEADLINE <- as.factor(build$HEADLINE)
 CLICKS <- build$CLICKS
 
-#asdasfa
+AD_ID.levels <- levels(AD_ID)
+AD_ID.levels <- AD_ID.levels[AD_ID.levels != ""]
+
+HEADLINE.levels <- levels(HEADLINE)
+HEADLINE.levels <- HEADLINE.levels[HEADLINE.levels != ""]
